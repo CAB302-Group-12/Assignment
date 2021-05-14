b00a287ee2ae32eaa2dcac4b9d6dee76de99aae9
VaneLiang <53897596+VaneLiang@users.noreply.github.com>
2021-05-14 18:29:06 +0800

    Database for User and Asset

---
+++ Assignment_try_database_3/src/serialisationExercise/AssetInfo.java
@@ -0,0 +1,36 @@
+package serialisationExercise;
+
+import common.AssetInfoData;
+import common.AssetInfoUI;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Invokes the Address Book application.
+ * @author Malcolm Corney
+ * @version $Id:  Exp $
+ */
+public class AssetInfo {
+
+    /**
+     * Create the GUI.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        new AssetInfoUI(new AssetInfoData(new AssetFileDataSource()));
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
---
+++ Assignment_try_database_3/src/serialisationExercise/UserInfo2.java
@@ -0,0 +1,36 @@
+package serialisationExercise;
+
+import common.UserInfoData;
+import common.UserInfoUI;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Invokes the Address Book application.
+ * @author Malcolm Corney
+ * @version $Id:  Exp $
+ */
+public class UserInfo2 {
+   
+   /**
+    * Create the GUI.  For thread safety,
+    * this method should be invoked from the
+    * event-dispatching thread.
+    */
+   private static void createAndShowGUI() {
+      new UserInfoUI(new UserInfoData(new UserFileDataSource()));
+   }
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+       //Schedule a job for the event-dispatching thread:
+       //creating and showing this application's GUI.
+       SwingUtilities.invokeLater(new Runnable() {
+           public void run() {
+               createAndShowGUI();
+           }
+       });
+   }
+}
