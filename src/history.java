0a7494058888e6d7e026f9dfa7971c44781ee6ab
JunweiYang927 <81400037+JunweiYang927@users.noreply.github.com>
2021-06-02 17:39:43 +1000

    Update history.java
    Use the GroupLayout to recreate the history GUI

--- src/history.java
+++ src/history.java
@@ -1,29 +1,38 @@
 import javax.swing.*;
-import javax.swing.border.BevelBorder;
-import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class history {
-    public static void main(String[] args) {
-        /**
-         * Create new JFrame "History"
-         */
-        JFrame jframe = new JFrame("History");
+public class history extends JFrame{
+
+    public history(){
+        initComponents();
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                closeFrame();
+            }
+
+        });
+        setTitle("Create History");
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void closeFrame() {
+        this.dispose();
+    }
+
+    private void initComponents() {
 
         /**
-         * Create new JPanel to use
-         */
-        JPanel panel = new JPanel();
-
-        /**
-         * Create the button "Cancel", button "Remove" and button "Edit"
-         * @param button
+         * Create the button "Back"
          */
         JButton button = new JButton("Back");
         button.setBounds(40, 250, 100, 40);
-        panel.add(button);
 
         /**
          * Create a table to edit one or some assets
+         *
          * @param OrderNumber
          * @param Organization
          * @param Buyer
@@ -31,39 +40,42 @@ public class history {
          * @param Quantity
          * @param Price
          */
-        String[] columnNames = {"OrderNum", "Organization", "Buyer", "Category", "Quantity", "Price"};
+        String[] columnNames = { "OrderNum", "Organization", "Buyer", "Category", "Quantity", "Price" };
 
         String[][] data = {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
+                { null, null, null, null, null, null },
+                { null, null, null, null, null, null },
+                { null, null, null, null, null, null },
+                { null, null, null, null, null, null },
+                { null, null, null, null, null, null }
         };
         JTable table = new JTable(data, columnNames);
 
         /**
          * Create scroll pane of table and set it
-         * @param scrollPane
          */
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setBounds(0, 0, 550, 210);
         scrollPane.setViewportView(table);
-        panel.add(scrollPane);
-        panel.setLayout(null);
 
-        /**
-         * Set the border for the jframe
-         */
-        panel.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
-        //Add panel to the jframe
-        jframe.getContentPane().add(panel);
-        //Set window size
-        jframe.setPreferredSize(new Dimension(550, 370));
-        //Close the window
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.pack();
-        jframe.setLocationRelativeTo(null);
-        jframe.setVisible(true);
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(scrollPane, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createSequentialGroup()
+                        .addComponent(button))
+
+        );
+
+        layout.setVerticalGroup(layout.createSequentialGroup().addComponent(scrollPane)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(button))
+                .addContainerGap());
+        pack();
+    }
+
+    public static void main(String[] args) {
+        new history();
     }
 }
