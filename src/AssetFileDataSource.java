79e135b7f7282615b8b013260b6465d658a66c74
JunweiYang927 <81400037+JunweiYang927@users.noreply.github.com>
2021-06-02 17:32:33 +1000

    Update AssetFileDataSource.java
    Update the AssetFileDataSource

--- src/AssetFileDataSource.java
+++ src/AssetFileDataSource.java
@@ -1,49 +1,44 @@
-package com.computer.trading.client.ui.model;
-
-
+package Asset;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
-import com.computer.trading.client.controller.AssetController;
-import com.computer.trading.client.util.Configuration;
-import com.computer.trading.common.Constants;
-import com.computer.trading.common.beans.Asset;
-
 public class AssetFileDataSource implements AssetInfoDataSource {
-   
+    private static final String FILENAME = "asset.dat";
 
     /**
      * Storage for the asset objects, name is the unique key per asset.
      */
     private HashMap<String, Asset> data;
 
-    private AssetController  controller;
     /**
      * If any of the data has changed, this will be set to true.
      */
     private boolean dataChanged = false;
 
     public AssetFileDataSource() {
-        data = new HashMap<>();
-        controller = new AssetController(Configuration.getInstance(Constants.CLIENT_CONFIG_PATH));
+        try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream(FILENAME))) {
+            // recursively read in the objects from the hash map (keys and people)
+            data = (HashMap<String, Asset>) stream.readObject();
+        } catch (IOException | ClassNotFoundException | ClassCastException e) {
+            // if the file does not exist (we're running for the first time)
+            // or the serial number does not match (class has changed)
+            // start with an empty data store
+            data = new HashMap<>();
 
-        List<Asset> assets = controller.getAllAssets();
-        for(Asset a: assets) {
-        	data.put(a.getName(), a);
+            // we definitely want to save this data, because we're started empty
+            dataChanged = true;
         }
-        
     }
 
     @Override
-    public void addAsset(Asset p) {
-        if (p == null)
+    public void addAsset(Asset a) {
+        if (a == null)
             throw new IllegalArgumentException("Asset cannot be null");
 
         // map[name] = p
-        data.put(p.getName(), p);
+        data.put(a.getName(), a);
         dataChanged = true;
     }
 
@@ -61,23 +56,14 @@ public class AssetFileDataSource implements AssetInfoDataSource {
     }
 
     @Override
-    public void deleteAsset(String name) {
-        // remove the asset from the map with this name (or key)
-        data.remove(name);
-        dataChanged = true;
-    }
-
-    @Override
     public void close() {
         // has the data changed? do we need to update the file?
         if (!dataChanged)
             return;
 
-        /**
-         * open up the stream for writing the objects to
-         * if this file already exists, it will be overwritten
-         */
-        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(Constants.ASSET_PATH))) {
+        // open up the stream for writing the objects to
+        // if this file already exists, it will be overwritten
+        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(FILENAME))) {
             // save the hash map to the file
             // it's recursive in this case, so it saves all the keys as well as the values (Asset)
             stream.writeObject(data);
@@ -93,7 +79,7 @@ public class AssetFileDataSource implements AssetInfoDataSource {
 
     @Override
     public Set<String> nameSet() {
-     
+        // return the keys (the people's names) from the hash map
         return data.keySet();
-    }      
+    }
 }
